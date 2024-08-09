399e232915ef27fbca7223b98f81a11a66488881
zatomis <zatomis@list.ru>
2024-08-09 14:46:55 +0300

    Отправка данных клиенту

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
+from datetime import datetime
+
+import aiofiles
 import asyncio
+from aiohttp import web
+
+INTERVAL_SECS = 1
+
 
 async def get_zip():
     args = ["img.png", "img_1.png", "img_2.png", "img_3.png"]
@@ -17,5 +24,39 @@ async def get_zip():
         file.write(bytes_zip)
 
 
+
+async def archive(request):
+
+    # raise NotImplementedError
+    response = web.StreamResponse()
+
+    # Большинство браузеров не отрисовывают частично загруженный контент, только если это не HTML.
+    # Поэтому отправляем клиенту именно HTML, указываем это в Content-Type.
+    response.headers['Content-Type'] = 'text/html'
+
+    # Отправляет клиенту HTTP заголовки
+    await response.prepare(request)
+
+    while True:
+        formatted_date = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+        message = f'{formatted_date}<br>'  # <br> — HTML тег переноса строки
+
+        # Отправляет клиенту очередную порцию ответа
+        await response.write(message.encode('utf-8'))
+
+        await asyncio.sleep(INTERVAL_SECS)
+
+async def handle_index_page(request):
+    async with aiofiles.open('index.html', mode='r') as index_file:
+        index_contents = await index_file.read()
+    return web.Response(text=index_contents, content_type='text/html')
+
+
 if __name__ == '__main__':
-    create_zip = asyncio.run(get_zip())
+    # create_zip = asyncio.run(get_zip())
+    app = web.Application()
+    app.add_routes([
+        web.get('/', handle_index_page),
+        web.get('/archive/{archive_hash}/', archive),
+    ])
+    web.run_app(app)
