ee8152d97e37666ce286d185fc9c19b94202b610
zatomis <zatomis@list.ru>
2024-08-10 12:23:11 +0300

    Отправка данных клиенту

--- main.py
+++ main.py
@@ -4,47 +4,32 @@ import aiofiles
 import asyncio
 from aiohttp import web
 
-INTERVAL_SECS = 1
 
 
-async def get_zip():
-    args = ["img.png", "img_1.png", "img_2.png", "img_3.png"]
-    command = ["zip", "-r", "-", *args]
+async def archive(request):
+    response = web.StreamResponse()
+    response.content_type = 'application/zip'
+    response.content_disposition = 'attachment; filename="files.zip"'
+    await response.prepare(request)
+
+    # request.url.parts[2]
+    # args = ["*"]
+    # command = ["zip", "-j", f"photos/{request.url.parts[2]}/*", "-r", "-",]
+    command = ["zip", "-r", "-", "-j", f"photos/{request.url.parts[2]}/.*"]
     proc = await asyncio.create_subprocess_exec(*command, stdout = asyncio.subprocess.PIPE, stderr = asyncio.subprocess.PIPE)
-    bytes_zip = b''
     while True:
         if proc.stdout.at_eof():
             break
         stdout = await proc.stdout.read(100)
-        if stdout:
-            print(f'[stdout] {stdout}')
-            bytes_zip += stdout
+        await response.write(stdout)
     await proc.wait()
-    with open("zip_file.zip", "wb") as file:
-        file.write(bytes_zip)
+    return response
 
-
-
-async def archive(request):
-
-    # raise NotImplementedError
-    response = web.StreamResponse()
-
-    # Большинство браузеров не отрисовывают частично загруженный контент, только если это не HTML.
-    # Поэтому отправляем клиенту именно HTML, указываем это в Content-Type.
-    response.headers['Content-Type'] = 'text/html'
-
-    # Отправляет клиенту HTTP заголовки
-    await response.prepare(request)
-
-    while True:
-        formatted_date = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-        message = f'{formatted_date}<br>'  # <br> — HTML тег переноса строки
-
-        # Отправляет клиенту очередную порцию ответа
-        await response.write(message.encode('utf-8'))
-
-        await asyncio.sleep(INTERVAL_SECS)
+    # while True:
+    #     formatted_date = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    #     message = f'{formatted_date}<br>'  # <br> — HTML тег переноса строки
+    #     await response.write(message.encode('utf-8'))
+    #     await asyncio.sleep(INTERVAL_SECS)
 
 async def handle_index_page(request):
     async with aiofiles.open('index.html', mode='r') as index_file:
@@ -53,7 +38,6 @@ async def handle_index_page(request):
 
 
 if __name__ == '__main__':
-    # create_zip = asyncio.run(get_zip())
     app = web.Application()
     app.add_routes([
         web.get('/', handle_index_page),
