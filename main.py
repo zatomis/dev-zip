a1f836298642fe35ebf1adc7182796a8b3ca4210
zatomis <zatomis@list.ru>
2024-08-17 14:58:30 +0300

    Bug fix

--- main.py
+++ main.py
@@ -36,46 +36,36 @@ def parse_arguments():
     return args
 
 
-async def archive(request, log, folder, delay):
+async def archive(request, folder, delay):
     response = web.StreamResponse()
     work_dir = f"{folder}/{request.url.parts[2]}/"
     if os.path.isdir(work_dir):
+        logger.info(f"Send archive from  - {work_dir}")
+        response.content_type = 'application/zip'
+        response.headers['Content-Disposition'] = 'attachment; \
+            filename="filename.zip"'
+        await response.prepare(request)
+        files_in_dir = os.listdir(f"./{work_dir}")
+        command = ["zip", "-r", "-", *files_in_dir]
+        proc = await asyncio.create_subprocess_exec(*command,
+                                                    stdout=asyncio.subprocess.PIPE,
+                                                    stderr=asyncio.subprocess.PIPE,
+                                                    cwd=work_dir)
+        logger.info(f"Pid = {proc.pid}")
         try:
-            if log:
-                logger.info(f"Send archive from  - {work_dir}")
-            response.content_type = 'application/zip'
-            response.headers['Content-Disposition'] = 'attachment; \
-                filename="filename.zip"'
-            await response.prepare(request)
-            files_in_dir = os.listdir(f"./{work_dir}")
-            command = ["zip", "-r", "-", *files_in_dir]
-            proc = await asyncio.create_subprocess_exec(*command,
-                                                        stdout=asyncio.subprocess.PIPE,
-                                                        stderr=asyncio.subprocess.PIPE,
-                                                        cwd=work_dir)
-            while True:
-                if proc.stdout.at_eof():
-                    break
+            while not proc.stdout.at_eof():
                 stdout = await proc.stdout.read(100)
-                if log:
-                    logger.info(f"Read bytes - {stdout}")
+                logger.info(f"Read bytes - {stdout}")
                 await asyncio.sleep(delay)
                 await response.write(stdout)
             await proc.wait()
             return response
-        except TimeoutExpired:
+        except (asyncio.CancelledError, TimeoutExpired):
+            logger.warning(f"Download was interrupted. Kill process №{proc.pid}")
             proc.kill()
-        except asyncio.CancelledError:
-            if log:
-                logger.info(f"Download was interrupted. Kill process №{proc.pid}")
-            command = ["kill", "-9", f"{proc.pid}"]
-            await asyncio.create_subprocess_exec(*command,
-                                                 stdout=asyncio.subprocess.PIPE,
-                                                 stderr=asyncio.subprocess.PIPE)
             raise
     else:
-        if log:
-            logger.warning(f"По указанному пути {work_dir}. Архив не существует или был удален.")
+        logger.warning(f"По указанному пути {work_dir}. Архив не существует или был удален.")
         response.headers['Content-Type'] = 'text/html'
         await response.prepare(request)
         message = f'По указанному пути <br>Архив не существует или был удален.<br>'
@@ -90,15 +80,23 @@ async def handle_index_page(request):
 
 
 def main():
-    logging.basicConfig(
-        format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO
-    )
+    parsed_arguments = parse_arguments()
+    if parsed_arguments.logging:
+        logging.basicConfig(
+            format='%(filename)s:%(lineno)d - %(levelname)-8s - %(asctime)s - %(funcName)s - %(name)s - %(message)s',
+            level=logging.INFO
+        )
+    else:
+        logging.basicConfig(
+            format='%(filename)s:%(lineno)d - %(levelname)-8s - %(asctime)s - %(funcName)s - %(name)s - %(message)s',
+            level=logging.WARNING
+        )
+
     parsed_arguments = parse_arguments()
     app = web.Application()
     app.add_routes([
         web.get('/', handle_index_page),
         web.get('/archive/{archive_hash}/', partial(archive,
-                                                    log=parsed_arguments.logging,
                                                     folder=parsed_arguments.dest_folder,
                                                     delay=parsed_arguments.delay_answer)),
     ])
